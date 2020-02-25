fe3b75273ce813d078867d4b72d17be34d58b892
Aman Ibrahim <amanmibra@gmail.com>
2020-02-25 13:21:30 -0500

    Added cards

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,36 +1,62 @@
 import React, { Component } from 'react';
 
-import { Card, Feed } from 'semantic-ui-react';
+import { Card, Grid, Feed } from 'semantic-ui-react';
 
 class Results extends Component {
 
   renderResults = () => {
     const { results } = this.props;
     const keys = Object.keys(results)
-    console.log(keys);
-    // debugger;
+    const calories = results.calories;
     return (
       <div>
-        {keys.map((key) => (
-          <div id={key}>
-            {key}: {results[key]}
-            <br />
-            <br />
-          </div>
-        ))}
+        <p><b>Calories: </b> {calories}</p>
+        <Feed>
+            {keys.map((key) => {
+              if (key == 'calories') {
+                return null
+              }
+              return (
+                <Feed.Event key={key}>
+                  <Feed.Content>
+                    <Grid centered columns={2}>
+                      <Grid.Column>{this.titleCase(key)}</Grid.Column>
+                      <Grid.Column>{results[key]}</Grid.Column>
+                    </Grid>
+                  </Feed.Content>
+                </Feed.Event>
+              );
+            })}
+        </Feed>
       </div>
     );
   }
 
+  titleCase = str => {
+    let splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+ }
+
   render() {
     const { results } = this.props;
     const keys = Object.keys(results)
+    const cardStyle = { color: 'black' }
     return (
-      <div>
-        {keys.length > 0 ? this.renderResults() :
-          <Card.Description>Search an item to get results...</Card.Description>
-        }
-      </div>
+      <Card
+        centered
+        fluid
+        style={cardStyle}
+      >
+        <Card.Content>
+          {keys.length > 0 ? this.renderResults() :
+            <Card.Description>Search an item to get results...</Card.Description>
+          }
+        </Card.Content>
+      </Card>
     );
   }
 
